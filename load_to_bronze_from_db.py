3992e435d84bae49cd5bcc4b9d1fdf917ac5b101
Rommagcom <86300034+Rommagcom@users.noreply.github.com>
2021-08-24 21:59:10 +0600

    Added impotr to dwh function

---
+++ load_to_bronze_from_db.py
@@ -0,0 +1,49 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from datetime import datetime
+import os
+import yaml
+
+from functions.load_to_bronze_silver import load_to_bronze
+
+default_args = {
+    "owner": "airflow",
+    "email_on_failure": False
+}
+
+
+
+def return_tables():
+    this_folder = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(this_folder,'configs','config.yaml'),'r') as yaml_file:
+        config = yaml.safe_load(yaml_file)
+        return config.get('daily_etl').get('sources').get('postgresql')
+        
+        
+def load_to_bronze_group(value):
+    return PythonOperator(
+        task_id="load_"+value+"_to_bronze",
+        python_callable=load_to_bronze,
+        op_kwargs={"table": value},
+        provide_context=True,
+    )
+        
+dag = DAG(
+    dag_id="load_to_bronze_from_db",
+    description="Load data from PostgreSQL dshop database to Data Lake bronze",
+    schedule_interval="0 0 * * *",
+    start_date=datetime(2021, 8, 11)
+)
+
+dummy1 = DummyOperator(
+    task_id='start_load_to_bronze',
+    dag=dag
+)
+dummy2 = DummyOperator(
+    task_id='finish_load_to_bronze',
+    dag=dag
+)
+
+for table in return_tables():
+    dummy1 >> load_to_bronze_group(table) >> dummy2
